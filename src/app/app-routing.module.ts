1427364dad906ced39f7afaf31d1271e1162d3fd
Robert Trojan <robert9620@gmail.com>
2019-12-12 23:55:01 +0100

    Initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {HowComponent} from './how/how.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/strona-glowna',
+    pathMatch: 'full'
+  },
+  {
+    path: 'strona-glowna',
+    component: IndexComponent
+  },
+  {
+    path: 'jak-to-dziala',
+    component: HowComponent
+  },
+  {
+    path: 'logowanie',
+    component: LoginComponent
+  },
+  {
+    path: 'rejestracja',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/strona-glowna'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+import { HowComponent } from './how/how.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MenuComponent,
+    IndexComponent,
+    FooterComponent,
+    HowComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MenuComponent]
 })
 export class AppModule { }
